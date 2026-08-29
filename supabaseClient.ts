e52599427341011d8360b54eefbd07f67bbbfdfc
didimusbeweisang <didimusbeweisang@gmail.com>
2026-08-29 08:27:59 +0100

    Delete supabaseClient.ts

--- supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://cgyisvdjnvfbkuidtigh.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNneWlzdmRqbnZmYmt1aWR0aWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2NzcxNTMsImV4cCI6MjA5NjI1MzE1M30.qu2tUJr2o_RpAtv6INdt0jH0bdvOCZBSngjaVHLJAaA'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
